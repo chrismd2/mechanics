c11f4225cb5a62d548878fc951d4ada2c3e67a72
chris christenson <charlie.mike.1991@gmail.com>
2026-03-15 19:12:26 -0500

    Enhance home page tests in PageControllerTest
    - Refactored existing test for the home page to include multiple assertions.
    - Added tests for tagline, mechanics for hire section, jobs available section, and sign up/sign in links.
    - Improved test coverage to ensure core functionalities are validated.

--- test/mechanics_web/controllers/page_controller_test.exs
+++ test/mechanics_web/controllers/page_controller_test.exs
@@ -1,8 +1,41 @@
 defmodule MechanicsWeb.PageControllerTest do
   use MechanicsWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  describe "GET / shows a home page with core functionality of this page" do
+    test "Checking for tagline ", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert html_response(conn, 200) =~ "Connecting mechanics with customers to complete jobs"
+    end
+
+    test "Checking for mechanics for hire section", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert html_response(conn, 200) =~ "Mechanics for hire"
+    end
+
+    test "Checking for jobs available section", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert html_response(conn, 200) =~ "Jobs available"
+    end
+
+    test "Checking for sign up links", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      html = html_response(conn, 200)
+      parsed = Floki.parse_document!(html)
+      links = Floki.find(parsed, "a[href*=\"register\"]")
+      assert links != []
+      sign_up_link = Enum.find(links, &(Floki.text(&1) =~ "Sign up"))
+      assert sign_up_link != nil
+    end
+
+
+    test "Checking for sign in links", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      html = html_response(conn, 200)
+      parsed = Floki.parse_document!(html)
+      links = Floki.find(parsed, "a[href*=\"login\"]")
+      assert links != []
+      sign_in_link = Enum.find(links, &(Floki.text(&1) =~ "Sign in"))
+      assert sign_in_link != nil
+    end
   end
 end
